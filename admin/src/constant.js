9958a71effea77b73a110ed666ce90802d334d1c
gadisa kasaye <gadisaka04@gmail.com>
2026-02-06 06:56:59 +0300

    swsws

--- admin/src/constant.js
+++ admin/src/constant.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:5000";
 export const API_URL = "https://onlinebingo-2yxx.onrender.com";
 
-export const FRONTEND_URL = "http://localhost:5173";
-// export const FRONTEND_URL = "https://mobilebingo.vercel.app";
+// export const FRONTEND_URL = "http://localhost:5173";
+export const FRONTEND_URL = "https://play-ethiobingo.vercel.app";
